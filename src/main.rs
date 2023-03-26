43503c20120c20bf673e86db18418866d332111b
nkct <miloszzegota@gmail.com>
2023-03-27 00:26:06 +0200

    added variable ip handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 use std::{
     thread,
     time::Duration,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    io::{prelude::*, BufReader, stdin},
+    net::{TcpListener, TcpStream, SocketAddr}, 
+    panic,
 };
+use regex::Regex;
 
 fn main() {
+    println!("Enter peer ip adress and port");
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    input.pop();
+
+    let ip_pattern = Regex::new(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}$").unwrap();
+    if !ip_pattern.is_match(&input) {
+        panic!("Invalid ip or port for {:?}.", &input);
+    }
+
     thread::spawn(move|| {
-        let mut connection = TcpStream::connect("127.0.0.1:7878").unwrap();
         loop {
-            connection.write(b"Request").unwrap();
+            if let Ok(mut connection) = TcpStream::connect_timeout(&input.as_str().parse::<SocketAddr>().unwrap(), Duration::from_millis(500)) {
+                println!("Connected on {}.", connection.peer_addr().unwrap());
+
+                connection.write(b"Request").unwrap();
+                println!("Pinged {}.", &input);
+            } else {
+                println!("Couldn't connect to peer.")
+            }
+            
 
             thread::sleep(Duration::from_millis(3000))
         }
     });
 
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let server_addr = "0.0.0.0:7878";
+    let listener = TcpListener::bind(server_addr).unwrap();
+    println!("Bound on {}.", server_addr);
 
     for stream in listener.incoming() {
         match stream {
