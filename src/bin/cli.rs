56a2a5f2d3c60155ae584fffb4df9729f228b853
nkct <miloszzegota@gmail.com>
2023-10-22 03:54:03 +0200

    added response checking to cli frontend

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,4 +1,4 @@
-use std::{io::{stdin, BufWriter, prelude::*}, net::TcpStream, env};
+use std::{io::{stdin, BufWriter, prelude::*, BufReader}, net::TcpStream, env};
 use ap2pmsg::*;
 
 fn main() {
@@ -10,12 +10,23 @@ fn main() {
     let server_addr = &args[1];
     let serv_conn = TcpStream::connect(server_addr).unwrap_or_else(|e| { panic!("ERROR: Could not connect to server, {}", e) });
 
+    let mut serv_writer = BufWriter::new(serv_conn.try_clone().unwrap());
+    let mut serv_reader = BufReader::new(serv_conn);
+
+    let mut response = String::new();
+    serv_reader.read_line(&mut response).unwrap();
+    match &response[..4] {
+        "OK: " => { println!("{}", &response[4..]) },
+        "ER: " => { panic!("ERROR: {}", &response[4..]) },
+        _ => { panic!("ERROR: Invalid backend response") }
+    }
+
+
     let mut input = String::new();
     loop {
         println!("Enter request to server: ");
         stdin().read_line(&mut input).unwrap();
 
-        let mut serv_writer = BufWriter::new(serv_conn.try_clone().unwrap());
         let request = serde_json::to_string(
             &ServerRequest::Send((
                 server_addr.parse().unwrap(), 
