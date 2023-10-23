0cba28417786156d48ba5734cfe85cbea3a7b59f
nkct <miloszzegota@gmail.com>
2023-10-23 18:32:05 +0200

    added Writable trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,28 @@ use std::{net::{SocketAddr, TcpStream}, io::{BufWriter, self, Write}};
 use serde::{Serialize, Deserialize};
 use time::OffsetDateTime;
 
+pub trait Writable {
+    fn write(&self, writer: &mut BufWriter<TcpStream>) -> Result<(), io::Error> where Self: Serialize {
+        let send = serde_json::to_string(
+            self
+        )? + "\n";
+        writer.write(send.as_bytes())?;
+        writer.flush()?;
+        Ok(())
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub enum BackendRequest {
     Send((SocketAddr, MessageContent)),
 }
+impl Writable for BackendRequest {}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum BackendResponse {
     ConnectionEstablished(Result<(), String>)
 }
-impl BackendResponse {
-    pub fn write(self, writer: &mut BufWriter<TcpStream>) -> Result<(), io::Error> {
-        let response = serde_json::to_string(
-            &self
-        )? + "\n";
-        writer.write(response.as_bytes())?;
-        writer.flush()?;
-        Ok(())
-    }
-}
+impl Writable for BackendResponse {}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum MessageContent {
