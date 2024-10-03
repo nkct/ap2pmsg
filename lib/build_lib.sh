8720e8d8361235cf26eac9706d6be3dc003a7432
nkct <miloszzegota@gmail.com>
2024-10-04 00:04:50 +0200

    rework project structure

---
+++ lib/build_lib.sh
@@ -0,0 +1,6 @@
+LIB_PATH=$(dirname "$0")
+
+echo -e "\x1b[32mBuilding\x1b[0m $LIB_PATH/libap2p.c"
+gcc -c -o "$LIB_PATH/libap2p.o" "$LIB_PATH/libap2p.c"
+ar rcs "$LIB_PATH/libap2p.a" "$LIB_PATH/libap2p.o"
+rm -f "$LIB_PATH/libap2p.o"
---
+++ lib/libap2p.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void ap2p_print_hello() {
+    printf("Hello from C!\n");
+}
